faf789c1e48d6315c4fd8c7b50f7a1a1f15eaa3f
ZJJ-XYH <zhoujunjie1031@126.com>
2022-06-01 15:22:37 +0800

    add 999 milliseconds up to consumption time

--- scripts/maas-script.js
+++ scripts/maas-script.js
@@ -3,7 +3,6 @@ const hre = require("hardhat");
 async function testInvokeSpeed() {
     // We get the deployed contract
     const contract = await hre.ethers.getContractAt("Storage", "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512");
-    const [gov, user1, user2, rewardAdder] = await hre.ethers.getSigners();
     
     var txlist = [];
 
@@ -13,12 +12,13 @@ async function testInvokeSpeed() {
         tx.sendTime = startTime.getTime();
         console.log("[" + new Date() + "]" + tx.hash);
         txlist.push(tx);
+        
     }
 
     for (var i = 0; i < txlist.length; i++) {
         var tx = txlist[i];
         var receipt = await tx.wait();
-        tx.endTime = (await contract.provider.getBlock(receipt.blockNumber)).timestamp * 1000;
+        tx.endTime = (await contract.provider.getBlock(receipt.blockNumber)).timestamp * 1000 + 999;
         console.log("[" + new Date() + "] tx hash: " + tx.hash + "  spend time: " + (tx.endTime - tx.sendTime));
     }
 }
